530d53096225118b47e09cc524be21a615b0fcc5
tsv2013 <tsv2013@users.noreply.github.com>
2023-03-19 15:16:34 +0300

    Fixed u-test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Abris Demo', () => {
+  render(<Provider store={store}>
+    <App />
+  </Provider>);
+  const tmElement = screen.getByText(/Abris Demo/i);
+  expect(tmElement).toBeInTheDocument();
 });
